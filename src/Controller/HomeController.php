3995b3b9b97209c960546c1105ad409996e4075a
Antoine LZCH <antoine@agence-osez.com>
2019-10-28 14:21:30 +0100

    display last products

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +12,16 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param ProductRepository $repository
      * @return Response
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
 
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
-        return $this->render("pages/home.html.twig");
+        $products = $repository->findLatest();
+        dump($products);
+        return $this->render("pages/home.html.twig", [
+            'products' => $products
+        ]);
     }
 }
